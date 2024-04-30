5a4d328c07908c462e0450aa71e28f9c17ff690c
matheuscmartins <matheus87martins@outlook.com>
2024-04-30 13:28:44 -0300

    logout created

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -8,10 +10,16 @@ import { Router } from '@angular/router';
 })
 export class NavComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+    private authService: AuthService,
+    private toastr: ToastrService) { }
 
   ngOnInit(): void {
-    this.router.navigate(['adress'])
+    this.router.navigate(['home'])
   }
-
+  logout(){
+    this.router.navigate(['login'])
+    this.authService.logout();
+    this.toastr.info('Logout efetuado com sucesso!', 'Logout', {timeOut: 7000})
+  }
 }
