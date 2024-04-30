362e62bfa1b2ab8f28d64312683ae42ef932cc32
matheuscmartins <matheus87martins@outlook.com>
2024-04-30 10:04:46 -0300

    service msg implemented

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 import { Credentials } from 'src/app/models/credentials';
 
 @Component({
@@ -16,10 +17,16 @@ export class LoginComponent implements OnInit {
   email = new FormControl(null, Validators.email);
   password = new FormControl(null, Validators.minLength(6));
 
-  constructor() { }
+  constructor(private toast: ToastrService) { }
 
   ngOnInit(): void {
   }
+
+  logar(){
+    this.toast.error('E-mail e/ou senha inválidos!', 'Login');
+    this.creds.password ='';
+  }
+
   validaCampos(): boolean{
     if(this.email.valid && this.password.valid){
       return true;
